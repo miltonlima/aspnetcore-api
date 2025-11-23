8f55e37c71d1a63e990aafecf73a734f97522efd
Milton Lima <milton.mlima@gmail.com>
2025-11-23 01:23:49 -0300

    atualiza informações de versão e arquivos binários do projeto

--- obj/Debug/net9.0/aspnetcore-api.AssemblyInfo.cs
+++ obj/Debug/net9.0/aspnetcore-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aspnetcore-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abe98bd2902cd5f8f2e7a89c6fa8823f0d771ec3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afc931ceb76d73ed2168aed0a6dcfe9fece029a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("aspnetcore-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aspnetcore-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
